809a0b6c429eefea5e29bf5f7661a5f41e9069f5
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2025-12-16 17:30:20 +0300

    refactor (files folder names)

---
+++ Assets/_Project/Systems/CombatAndTraversalSystem/Player/StateMachines/PlayerBaseState.cs
@@ -0,0 +1,42 @@
+﻿using _Project.Systems.CombatAndTraversalSystem.Targeting;
+using _Project.Systems.Core.StateMachine;
+using UnityEngine;
+
+namespace _Project.Systems.CombatAndTraversalSystem.Player.StateMachines
+{
+    public abstract class PlayerBaseState : State
+    {
+        protected readonly PlayerStateMachine stateMachine;
+
+        protected PlayerBaseState(PlayerStateMachine stateMachine)
+        {
+            this.stateMachine = stateMachine;
+        }
+
+        protected void Move(Vector3 movement, float deltaTime)
+        {
+            var motion = stateMachine.ForceReceiver.Movement;
+            stateMachine.Controller.Move((motion + movement) * deltaTime);
+        }
+
+        protected void Move(float deltaTime) //, Vector3 attackForceTargetPos, float AttackForce
+        {
+            // stateMachine.Controller.Move(attackForceTargetPos * (AttackForce * deltaTime));
+            // var motion = stateMachine.ForceReceiver.Movement;
+            //
+            // stateMachine.Controller.Move(motion * deltaTime);
+            Move(Vector3.zero, deltaTime);
+
+        }
+
+        protected void FaceTarget(Target currentTarget, float deltaTime)
+        {
+            if (currentTarget == null) return;
+            Vector3 targetLookPos = currentTarget.transform.position - stateMachine.transform.position;
+            targetLookPos.y = 0; //if the player above or below the target we dont care..
+            Quaternion targetRotation = stateMachine.transform.rotation = Quaternion.LookRotation(targetLookPos);
+            Quaternion.Slerp(stateMachine.transform.rotation, targetRotation,
+                (stateMachine.rotationDampTime * deltaTime));
+        }
+    }
+}
