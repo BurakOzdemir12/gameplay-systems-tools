c49eb3a3e06cff2a47ceabf40168a6b29a470bee
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-01-05 22:30:42 +0300

    refactor: Redesign folder structure

---
+++ Assets/_Project/Systems/CombatSystem/Player/States/PlayerBlockingState.cs
@@ -0,0 +1,67 @@
+﻿using _Project.Systems._Core.StateMachine.Player;
+using _Project.Systems.MovementSystem.Player.States;
+using _Project.Systems.MovementSystem.Player.States.RootStates;
+using UnityEngine;
+
+namespace _Project.Systems.CombatSystem.Player.States
+{
+    public class PlayerBlockingState : PlayerBaseState
+    {
+        public PlayerBlockingState(PlayerStateMachine stateMachine) : base(stateMachine)
+        {
+        }
+
+        private float blockLayerWeight;
+        private const string BLOCK_TAG = "Block";
+        private PlayerGroundedState GroundParent => GetSuperState() as PlayerGroundedState;
+
+        public override void Enter()
+        {
+            stateMachine.Animator.SetBool(stateMachine.IsBlockingBoolHash, true);
+            blockLayerWeight = stateMachine.Animator.GetLayerWeight(stateMachine.BlockingLayerIndex);
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            float rollDampTime = stateMachine.RotationDampTimeWhileBlock;
+
+            RotateFaceToLook(deltaTime, rollDampTime);
+            Move(deltaTime);
+            float target = stateMachine.InputHandler.IsBlocking ? 1f : 0f;
+            float normalizedTime = GetNormalizedTime(stateMachine.Animator, 1, BLOCK_TAG);
+
+            if (!stateMachine.InputHandler.IsBlocking)
+            {
+                stateMachine.Animator.SetBool(stateMachine.IsBlockingBoolHash, false);
+                blockLayerWeight =
+                    Mathf.MoveTowards(blockLayerWeight, target, deltaTime * stateMachine.BlockingLayerChangeSpeed);
+                stateMachine.Animator.SetLayerWeight(stateMachine.BlockingLayerIndex, blockLayerWeight);
+
+                if (normalizedTime >= 1f)
+                {
+                    if (stateMachine.Targeter.SelectedTarget != null)
+                    {
+                        GroundParent.SwitchSubState(new PlayerTargetingState(stateMachine));
+                    }
+                    else
+                    {
+                        GroundParent.SwitchSubState(new PlayerFreeLookState(stateMachine));
+                    }
+                    // stateMachine.DecideTargetOrLocomotion();
+                }
+
+                return;
+            }
+
+            blockLayerWeight =
+                Mathf.MoveTowards(blockLayerWeight, target, deltaTime * stateMachine.BlockingLayerChangeSpeed);
+            stateMachine.Animator.SetLayerWeight(stateMachine.BlockingLayerIndex, blockLayerWeight);
+        }
+
+        public override void Exit()
+        {
+            stateMachine.Animator.SetBool(stateMachine.IsBlockingBoolHash, false);
+            stateMachine.Animator.SetLayerWeight(stateMachine.BlockingLayerIndex, 0f);
+        }
+    }
+}
