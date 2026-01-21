438c7bd19a5d9d12e67259a0aa718d7e975b7059
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-01-21 22:50:09 +0300

    refactor: Field of view for Enemy chase

--- Assets/_Project/Systems/_Core/StateMachine/Player/PlayerStateMachine.cs
+++ Assets/_Project/Systems/_Core/StateMachine/Player/PlayerStateMachine.cs
@@ -6,7 +6,6 @@ using _Project.Systems._Core.GroundCheck;
 using _Project.Systems._Core.Health;
 using _Project.Systems._Core.HitboxLogic;
 using _Project.Systems._Core.Pickup_Drop;
-using _Project.Systems._Core.ScriptableObjects.Characters;
 using _Project.Systems._Core.Weapon_Tool_Handlers;
 using _Project.Systems.ClimbingSystem.LedgeClimbing;
 using _Project.Systems.ClimbingSystem.ScriptableObjects;
